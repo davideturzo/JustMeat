f3433a2e9975df3069b485f753ae089b5dd95733
stealth90 <41144486+stealth90@users.noreply.github.com>
2019-11-25 18:12:57 +0100

    replace routes of order and fix some api calls

--- src/app.ts
+++ src/app.ts
@@ -2,114 +2,16 @@ import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import * as user from './user';
 import * as rest from './restaurant'
-let order = require('./routes/order');
+let orders = require('./routes/ordersAPI');
+let users = require('./routes/usersAPI');
+let restaurants = require('./routes/restaurantsAPI');
 const app: Application = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/orders/',order)
-app.post('/user/create', (req: Request, res: Response) => {
-    if(!isNaN(req.body.username && req.body.name && req.body.surname)){
-       return res.status(400).send("username, name, surname must be valid");
-    }
-    if(!isNaN(req.body.address && req.body.phone && req.body.email)){
-        return res.status(400).send("address, phone and email must be valid");
-    }
-    return res.json(user.newUser(req.body));
-});
+app.use('/orders/',orders)
+app.use('/users/',users)
+app.use('/restaurants/',restaurants)
 
-app.get('/users', (req: Request, res: Response) => {
-    if(!isNaN(req.query.username)){
-        res.status(400).send("Username must be valid");
-    }
-    if(req.query.username){
-       return res.json(user.userById(req.query.username));
-    } 
-    return res.json(user.usersList());
-});
-
-//TODO put call must be fixed, it produce a strange type of error
-app.put('/users/:username', (req: Request, res: Response) => {
-    if(req.params.username){
-        return res.json(user.updateUserFields(req.params.username, req.body.password, req.body.name, req.body.surname, req.body.address, req.body.phone, req.body.email));
-    } else {
-        return res.status(400).send("You should change one field");
-    }
-});
-
-app.delete('/users/:id', (req: Request, res: Response) => {
-    if(req.params.id){
-        return res.json(user.deleteUser(req.params.id));
-    } else {
-        return res.status(404).send("User not found");
-    }
-});
-
-app.post('/orders/create', (req: Request, res: Response) => {
-    return res.json(order.newOrder(req.body));
-});
-app.put('/orders/:id', (req: Request, res: Response) => {
-    return res.json(order.changeStatusOrder(req.params.id));
-});
-app.delete('/orders/:id', (req: Request, res: Response) => {
-    return res.json(order.deleteOrder(req.params.id));
-});
-app.put('/orders/change/:id', (req: Request, res: Response) => {
-    return res.json(order.changeRatingOrder(req.params.id,req.query.rating));
-});
-app.get('/orders', (req: Request, res: Response) => {
-    if(req.query.userId && req.query.restaurantId){
-        return res.json(order.getOrdersByBothId(req.query.userId,req.query.restaurantId));
-    }else if(req.query.namePlate){
-        return res.json(order.getOrdersByPlate(req.query.namePlate));
-    }
-    return res.json(order.getOrdersList());
-});
-app.get('/orders/:id', (req: Request, res: Response) => {
-    return res.json(order.getOrdersById(req.params.id));
-});
-app.get('/orders/:userId/expensiveOrder', (req: Request, res: Response) => {
-    return res.json(order.getExpensiveOrder(req.params.userId));
-});
-app.get('/orders/:userId/cheaperOrder', (req: Request, res: Response) => {
-    return res.json(order.getCheaperOrder(req.params.userId));
-});
-app.get('/orders/user/:id', (req: Request, res: Response) => {
-    return res.json(order.getOrdersByUserId(req.params.id));
-});
-app.get('/orders/restaurant/:id', (req: Request, res: Response) => {
-    return res.json(order.getOrdersByRestaurantId(req.params.id));
-});
-app.post('/restaurant/create', (req: Request, res: Response) => {
-    if(!isNaN(req.body.name && req.body.address && req.body.email)){
-        return res.status(400).send("name, address, email must be valid");
-     }
-     return res.json(rest.newRestaurant(req.body));
-});
-app.get('/restaurant/list', (req: Request, res: Response) => {
-    return res.json(rest.getRestaurantList());
-}); 
-app.put('/restaurant/update/:id', (req: Request, res: Response) => {
-        if(req.params.id){
-            if(req.body.name){
-                rest.updateRestaurantFields(req.params.id, req.body.name, req.body.address, req.body.email, req.body.plate);
-                return res.json(rest.getRestaurantList());
-            }
-        }
-});
-app.get('/restaurant/search/:id', (req: Request, res: Response) => {
-    if(req.params.id){
-        return res.json(rest.restaurantById(req.params.id));
-    }else{
-        return res.status(404).send('restaurant not found');
-    }
-});
-app.delete('/restaurant/delete/:id', (req: Request, res: Response) => {
-    if(req.params.id){
-        return res.json(rest.deleteRestaurant(req.params.id));
-    }else{
-        return res.status(404).send('Restaurant not found');
-    }
-})
 app.listen(3001, "Localhost", (err) => {
     if(err) {
         return console.log(err);
--- src/routes/ordersAPI.ts
+++ src/routes/ordersAPI.ts
@@ -5,52 +5,39 @@ const router: Router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
+router.post('/create', (req, res) => {
+    return res.json(order.newOrder(req.body));
+});
+router.put('/:id/acceptOrder', (req, res) => {
+    return res.json(order.changeStatusOrder(req.params.id));
+});
+router.put('/:id/putRating', (req, res) => {
+    return res.json(order.changeRatingOrder(req.params.id,req.query.rating));
+});
+router.delete('/delete/:id', (req, res) => {
+    return res.json(order.deleteOrder(req.params.id));
+});
+router.get('/', (req, res) => {
     if(req.query.userId && req.query.restaurantId){
         return res.json(order.getOrdersByBothId(req.query.userId,req.query.restaurantId));
     }else if(req.query.namePlate){
         return res.json(order.getOrdersByPlate(req.query.namePlate));
-    }
-    res.json(order.getOrdersList());
-    next();
+    }else if(req.query.userId){
+        return res.json(order.getOrdersById(req.query.userId));
+    }else
+    return res.json(order.getOrdersList());
 });
-
-router.post('/create', (req: Request, res: Response, next: NextFunction) => {
-    res.json(order.newOrder(req.body));
-    next();
+router.get('/:userId/expensiveOrder', (req, res) => {
+    return res.json(order.getExpensiveOrder(req.params.userId));
 });
-router.put('/:id', (req: Request, res: Response, next: NextFunction) => {
-    res.json(order.changeStatusOrder(req.params.id));
-    next();
+router.get('/:userId/cheaperOrder', (req, res) => {
+    return res.json(order.getCheaperOrder(req.params.userId));
 });
-router.delete('/:id', (req: Request, res: Response, next: NextFunction) => {
-    res.json(order.deleteOrder(req.params.id));
-    next();
+router.get('/user/:id', (req, res) => {
+    return res.json(order.getOrdersByUserId(req.params.id));
 });
-router.put('/change/:id', (req: Request, res: Response, next: NextFunction) => {
-    res.json(order.changeRatingOrder(req.params.id,req.query.rating));
-    next();
-});
-
-router.get('/:id', (req: Request, res: Response, next: NextFunction) => {
-    res.json(order.getOrdersById(req.params.id));
-    next();
-});
-router.get('/:userId/expensiveOrder', (req: Request, res: Response, next: NextFunction) => {
-    res.json(order.getExpensiveOrder(req.params.userId));
-    next();
-});
-router.get('/:userId/cheaperOrder', (req: Request, res: Response, next: NextFunction) => {
-    res.json(order.getCheaperOrder(req.params.userId));
-    next();
-});
-router.get('/user/:id', (req: Request, res: Response, next: NextFunction) => {
-    res.json(order.getOrdersByUserId(req.params.id));
-    next();
-});
-router.get('/restaurant/:id', (req: Request, res: Response, next: NextFunction) => {
-    res.json(order.getOrdersByRestaurantId(req.params.id));
-    next();
+router.get('/restaurant/:id', (req, res) => {
+    return res.json(order.getOrdersByRestaurantId(req.params.id));
 });
 
 export = router;
