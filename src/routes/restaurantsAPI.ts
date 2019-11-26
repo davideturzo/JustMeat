6aceb823c79a9279a20438ad82e1beeeb93a7457
Terranova Enrico <terranovaenrico@MacBook-Pro-2.local>
2019-11-26 13:00:33 +0100

    fix API && restaurant.ts

--- src/routes/restaurantsAPI.ts
+++ src/routes/restaurantsAPI.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response, NextFunction, Router} from 'express';
+import express, {Request, Response, NextFunction, Router, response} from 'express';
 import * as rest from '../restaurant';
 import bodyParser from 'body-parser';
 const router: Router = express.Router();
@@ -13,21 +13,21 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
     next();
 });
 
-router.post('/create', (req: Request, res: Response, next: NextFunction) => {
+router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
     if(!isNaN(req.body.name && req.body.address && req.body.email && req.body.plate && req.body.typology)){
         return res.status(400).send("name, address, email must be valid");
     }
-    res.json(rest.newRestaurant(req.body));
-    next();
+    const response = await rest.newRestaurant(req.body);
+    res.json({response});
 });
 
-router.put('/update/:id', (req: Request, res: Response, next: NextFunction) => {
+router.put('/update/:id', async (req: Request, res: Response, next: NextFunction) => {
     if(!req.params.id){
         return res.status(400).send('ID must be invalid');
     }
     if(req.body.name){
-        rest.updateRestaurantFields(req.params.id, req.body.name, req.body.address, req.body.email, req.body.plate);
-        return res.json(rest.getRestaurantList());
+        const response = await rest.updateRestaurantFields(req.params.id, req.body.name, req.body.address, req.body.email, req.body.plate);
+        return res.json(response);
     }
     next();
 });
