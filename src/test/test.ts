4217c5c7ffa42e8119645dab7ec7bf373a2d6aa4
stealth90 <41144486+stealth90@users.noreply.github.com>
2019-11-26 13:23:58 +0100

    added some test in test.ts

--- src/test/test.ts
+++ src/test/test.ts
@@ -4,7 +4,41 @@ import assert from 'assert';
 import users from '../routes/usersAPI';
 import * as usersMethod from '../user';
 import { userById, User, usersList } from '../user';
-// import app from '../app';
+
+const app = require('../app');
+
+
+describe('GET /orders', function () {
+    it('respond with json containing a list of all orders', function (done) {
+        request(app)
+            .get('/orders')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
+describe('POST /orders/create', function () {
+    let data = {
+        "userId" : "4e2fd5f0-0e03-11ea-93a5-213c75413eaa",
+        "restaurantId" : "5a1d5b20-0d41-4c26-8a21-0a5b6d9c3884",
+        "shippingAddress" : "Some value",
+        "orderItems" : [{
+            "quantity" : 1,
+            "namePlate" :"pizza"
+        }]
+    }
+    it('respond with json containing a order just created', function (done) {
+        request(app)
+            .post('/orders/create')
+            .send(data)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err) => {
+                if(err) return done(err);
+                done();
+            })
+    });
+});
 
 
 
